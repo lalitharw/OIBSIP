0c6bad1be1c6becd4e185895d638f80e1eb1e4ca
lalitharw <lalitharwate90@gmail.com>
2024-02-09 18:34:47 +0530

    new changes

--- BMI/app.py
+++ BMI/app.py
@@ -1,13 +1,14 @@
 def calcalute_BMI(height:float,weight:float):
         BMI = weight/(height**2)
         result = ""
+       #  bmi logic
         if(BMI < 18.5):
               result = "UnderWeight"  
         elif(BMI >= 18.5 and BMI <=24.9):
                result = "healthy"
-        elif(BMI >=30 and BMI <= 29.9):
+        elif(BMI >=30 and BMI <= 29.9): 
                result = "overWeight"
-        elif(BMI >=30 and BMI <= 34.9):
+        elif(BMI >=30 and BMI <= 34.9): 
                result = "obese"
         else:
                result = "extremely obese"
