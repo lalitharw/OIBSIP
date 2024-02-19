18a21899eb6c8cabd64698747ff09e3a107297a4
lalitharw <lalitharwate90@gmail.com>
2024-02-19 22:14:28 +0530

    Done with python task of oasis infobyte

---
+++ random_password_generator/password.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+def GeneratePassword(length,chars,nums,syms):
+    characters = "" 
+    if chars == "y":
+        characters += string.ascii_letters
+    if nums == 'y':
+        characters += string.digits
+    if syms == 'y':
+        characters += string.punctuation
+
+    password = ''
+
+    for _ in range(length):
+        password += random.choice(characters)
+    
+
+    print("="*int(length+20))
+    print(f"Your password is: {password}")
+
+
+# length = int(input("Enter the length of password: "))
+length = int(input("Enter length of password: "))
+chars = input("Do you want characters in your password?(Y/n) ").lower()
+nums = input("Do you want numbers in your password?(Y/n) ").lower()
+syms = input("Do you want symbols in your password?(Y/n) ").lower()
+
+
+GeneratePassword(length,chars,nums,syms)
